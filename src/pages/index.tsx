db9ecfcaa81292f38ba643af97f5209419a77d51
Shreeti Mohapatra <114981873+shivah12@users.noreply.github.com>
2024-07-06 13:08:50 +0530

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,16 @@
 import Head from "next/head";
-import Email from "@/components/Email";
-import Loader from "@/components/Loader";
-import SocialIcons from "@/components/SocialIcons";
-import Footer from "@/sections/Footer";
-import Hero from "@/sections/Hero";
-import Navbar from "@/sections/Navbar";
+import Email from "../components/Email";
+import Loader from "../components/Loader";
+import SocialIcons from "../components/SocialIcons";
+import Footer from "../sections/Footer";
+import Hero from "../sections/Hero";
+import Navbar from "../sections/Navbar";
 import React, { useState } from "react";
-import About from "@/sections/About";
-import Contact from "@/sections/Contact";
-import Projects from "@/sections/Projects";
-import Experience from "@/sections/Experience";
-import OtherProjects from "@/sections/OtherProjects";
+import About from "../sections/About";
+import Contact from "../sections/Contact";
+import Projects from "../sections/Projects";
+import Experience from "../sections/Experience";
+import OtherProjects from "../sections/OtherProjects";
 
 function Index() {
   const [isLoading, setIsLoading] = useState(true);
