f0340bf5b0604ac71feb217ae21d31b145c9e5e8
Shreeti Mohapatra <114981873+shivah12@users.noreply.github.com>
2024-07-06 13:31:20 +0530

    Update FractalTree.tsx

--- src/components/FractalTree.tsx
+++ src/components/FractalTree.tsx
@@ -1,4 +1,3 @@
-// src/components/FractalTree.tsx
 import React from 'react';
 import dynamic from 'next/dynamic';
 
@@ -9,7 +8,37 @@ const P5Wrapper = dynamic(() => import('./P5Wrapper'), {
 
 let angle: number;
 
-const Sketch = (p5: any) => {
+const Sketch1 = (p5: any) => {
+  p5.setup = () => {
+    p5.createCanvas(400, 400, "transparent");
+    angle = p5.PI / 4;
+    p5.stroke(255);
+  };
+
+  p5.draw = () => {
+    p5.clear();
+    p5.translate(200, p5.height);
+    angle = p5.map(p5.sin(p5.frameCount * 0.01), -1, 1, p5.PI / 2, p5.PI / 16); // vary the angle using sin()
+    branch(100);
+  };
+
+  const branch = (len: number) => {
+    p5.line(0, 0, 0, -len);
+    p5.translate(0, -len);
+    if (len > 4) {
+      p5.push();
+      p5.rotate(angle);
+      branch(len * 0.67);
+      p5.pop();
+      p5.push();
+      p5.rotate(-angle);
+      branch(len * 0.67);
+      p5.pop();
+    }
+  };
+};
+
+const Sketch2 = (p5: any) => {
   p5.setup = () => {
     p5.createCanvas(400, 400, "transparent");
     angle = p5.PI / 4;
@@ -40,8 +69,9 @@ const Sketch = (p5: any) => {
 };
 
 const FractalTree: React.FC = () => (
-  <div id="fractal-tree">
-    <P5Wrapper sketch={Sketch} />
+  <div className="fractal-trees-container">
+    <P5Wrapper sketch={Sketch1} />
+    <P5Wrapper sketch={Sketch2} />
   </div>
 );
 
