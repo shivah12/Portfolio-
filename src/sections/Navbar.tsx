76685d565a885701a264a73a4229204770a75044
Shreeti Mohapatra <114981873+shivah12@users.noreply.github.com>
2024-07-06 14:28:48 +0530

    Update Navbar.tsx

--- src/sections/Navbar.tsx
+++ src/sections/Navbar.tsx
@@ -123,7 +123,7 @@ function Navbar() {
               delay: 0.6,
             }}
           >
-            <Button text="Resume" link="http://localhost:3000/resume.pdf" />
+            <Button text="Resume" link="https://drive.google.com/file/d/1wZnoulQP4ZrZckM86YjLqCiVIfX8Mci6/view?usp=sharing" />
           </motion.div>
         </div>
       </div>
